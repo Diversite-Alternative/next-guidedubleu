82e2dd02a5302ae38e4921944561271f8f898904
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-04-03 23:42:58 +0200

    create job form completed

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,15 +1,30 @@
+import React from 'react'
 import CreateJobForm from '@/components/CreateJobForm';
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query';
+
 function AddJobPage() {
-  const queryClient = new QueryClient();
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <CreateJobForm />
-    </HydrationBoundary>
-  );
+    <div>
+      <CreateJobForm /> 
+    </div>
+  )
 }
-export default AddJobPage;
+
+export default AddJobPage
+
+
+
+// import CreateJobForm from '@/components/CreateJobForm';
+// import {
+//   dehydrate,
+//   HydrationBoundary,
+//   QueryClient,
+// } from '@tanstack/react-query';
+// function AddJobPage() {
+//   const queryClient = new QueryClient();
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <CreateJobForm />
+//     </HydrationBoundary>
+//   );
+// }
+// export default AddJobPage;
