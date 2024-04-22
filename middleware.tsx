3aebb616c63c0fe1e695795b550f7108d581daf5
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-04-22 20:54:38 +0200

    clerk authentication deactivated

--- middleware.tsx
+++ middleware.tsx
@@ -1,13 +1,29 @@
-import { authMiddleware } from '@clerk/nextjs';
+// import { authMiddleware } from '@clerk/nextjs';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ['/'],
+// export default authMiddleware({
+//   publicRoutes: ['/'],
 
-});
+// });
 
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+import { NextResponse } from 'next/server';
+
+// This function acts as a simple middleware that logs the path of each request
+export default function middleware(req) {
+  console.log('Handling request for:', req.nextUrl.pathname);
+
+  // Continue processing the request normally after middleware operations
+  return NextResponse.next();
+}
+
+// Optional: configure which paths this middleware applies to
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
